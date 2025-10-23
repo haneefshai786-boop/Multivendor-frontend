e720c3ef5def77f6f5395850e308f8326f895c5f
haneefshai786-boop <haneefshai786@gmail.com>
2025-10-23 14:41:21 +0530

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,1 +1,33 @@
+import { useEffect, useState } from "react";
+import { getProducts } from "../api/productApi";
+
+function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const data = await getProducts();
+        setProducts(data);
+      } catch (err) {
+        console.error("Error fetching products:", err);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>All Products</h1>
+      <ul>
+        {products.map((p) => (
+          <li key={p._id}>{p.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Products;
 
