061e818c87314ad72e4c7cefb5220922e058fadd
haneefshai786-boop <haneefshai786@gmail.com>
2025-10-23 10:55:11 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx,js,jsx}"
+  ],
+  theme: { extend: {} },
+  plugins: []
+}
 
