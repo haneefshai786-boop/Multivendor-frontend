8fd6d4dd8097e4ecf3b6769fa8a7b99d428b7931
haneefshai786-boop <haneefshai786@gmail.com>
2025-10-23 14:29:17 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,1 +1,9 @@
 
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://multivendor-backend-6ozb.onrender.com", // backend URL
+  withCredentials: true,
+});
+
+export default api;
