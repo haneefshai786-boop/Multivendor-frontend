f4d136e79312bb6f63c872b5429b099e5d56c37f
haneefshai786-boop <haneefshai786@gmail.com>
2025-10-24 07:15:03 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,22 +1,8 @@
 import axios from "axios";
 
-// Use environment variable from .env
-const API = import.meta.env.VITE_API_URL;
-
-const axiosInstance = axios.create({
-  baseURL: API, // automatically prefixes all requests
-  headers: {
-    "Content-Type": "application/json",
-  },
+const instance = axios.create({
+  baseURL: "https://multivendor-backend-6ozb.onrender.com", // your backend
+  withCredentials: true,
 });
 
-// Optional: handle token if you use authentication
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-export default axiosInstance;
+export default instance;
