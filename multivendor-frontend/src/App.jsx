c202f4ed693aff187fa8439d4c0b3ec71630aa75
haneefshai786-boop <haneefshai786@gmail.com>
2025-11-01 07:33:10 +0530

    Update App.jsx

--- multivendor-frontend/src/App.jsx
+++ multivendor-frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import Login from './pages/Auth/Login'
-import Register from './pages/Auth/Register'
-import Home from './pages/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/home" element={<Home />} />
-    </Routes>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
